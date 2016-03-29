ef77417481438390e53c9808a07abb773bfe1ef8
t-takaai <takoyaj@gmail.com>
2016-03-30 07:38:40 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo01
+[Creating a Single Page Todo App with Node and Angular](https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular)
---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+
+// set up
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// configuration
+mongoose.connect('');
